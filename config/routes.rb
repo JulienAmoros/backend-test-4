19a8d9de85d59cf45964fb7bc69119b2120df9ad
Julien AMOROS <julien.amoros@cloud-temple.com>
2018-04-28 20:31:44 +0200

    attempt fixing delete routed as get on production

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   scope path: 'call' do
     root to: 'call#index'
-    get '/:id', to: 'call#show'
     delete '/:id', to: 'call#delete'
+    get '/:id', to: 'call#show'
 
     post '/hook', to:'webhook#incoming'
-    # get '/hook', to:'webhook#incoming'
+
     post '/hook/message', to:'webhook#message'
     post '/hook/status', to: 'webhook#call_status'
   end
